8689c98010fd3ce454cae793fe30740f3ec31964
raf <rraf@tuta.io>
2024-09-13 23:55:22 +0100

    update: wrote dock and undock scripts

--- default.nix
+++ default.nix
@@ -1,5 +1,10 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 
+let
+  USER = "raf";
+
+  egpu_pci_id = "05:00.0";
+in
 {
   boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
   services.xserver.videoDrivers = [ "nvidia" ];
@@ -18,7 +23,6 @@
     powerManagement.finegrained = false;
     forceFullCompositionPipeline = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
-
     prime = {
       sync.enable = true;
       allowExternalGpu = true;
@@ -26,4 +30,31 @@
       amdgpuBusId = "PCI:193:0:0";
     };
   };
+
+  nixpkgs.overlays = [
+    (self: super: {
+      dock = pkgs.writeScriptBin "dock" ''
+        #!${pkgs.stdenv.shell}
+        echo 1 | tee /sys/bus/pci/rescan
+      '';
+    })
+    (self: super: {
+      undock = pkgs.writeScriptBin "undock" ''
+        #!${pkgs.stdenv.shell}
+        echo 1 | tee /sys/bus/pci/devices/0000:${egpu_pci_id}/remove
+      '';
+    })
+  ];
+
+  security.sudo = {
+    extraConfig = ''
+      ${USER} ALL=(ALL) NOPASSWD: ${pkgs.dock}/bin/dock
+      ${USER} ALL=(ALL) NOPASSWD: ${pkgs.undock}/bin/undock
+    '';
+  };
+
+  environment.systemPackages = with pkgs; [
+    dock
+    undock
+  ];
 }
