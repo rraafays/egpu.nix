a109b00af66680371feacaee9991f9423a162a7b
raf <rraf@tuta.io>
2025-01-17 18:36:29 +0000

    chore: added alias to nvidia-offload

--- default.nix
+++ default.nix
@@ -1,22 +1,32 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 
 let
   amdgpuBusIdHex = "193:0:0";
   nvidiaBusIdHex = "100:0:0";
 in
 {
+  environment.systemPackages = [
+    (pkgs.writeScriptBin "egpu" ''
+      #! ${pkgs.bash}/bin/bash
+      exec nvidia-offload "$@"
+    '')
+  ];
+
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     open = false;
     nvidiaSettings = true;
     modesetting.enable = true;
-    forceFullCompositionPipeline = true;
+    forceFullCompositionPipeline = false;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
     prime = {
-      offload.enable = true;
-      allowExternalGpu = true;
       amdgpuBusId = "PCI:${amdgpuBusIdHex}";
       nvidiaBusId = "PCI:${nvidiaBusIdHex}";
+      allowExternalGpu = true;
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
     };
     powerManagement = {
       enable = false;
