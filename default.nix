b823bb764dc2affaba19bca570d8443f26e2c709
raf <rraf@tuta.io>
2025-01-17 17:03:59 +0000

    fix: much simpler egpu config

--- default.nix
+++ default.nix
@@ -1,73 +1,23 @@
-{ pkgs, config, ... }:
+{ config, ... }:
 
 let
-  USER = "raf";
-
-  egpu_pci_id = "05:00.0";
+  amdgpuBusIdHex = "193:0:0";
+  nvidiaBusIdHex = "100:0:0";
 in
 {
-  boot = {
-    kernelParams = [
-      "pci=assign-busses,hpbussize=0x33,realloc,hpmemsize=128M,hpmemprefsize=1G"
-      "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
-    ];
-    kernelModules = [
-      "nvidia"
-      "nvidia-modeset"
-      "nvidia-drm"
-      "nvidia-uvm"
-    ];
-  };
   services.xserver.videoDrivers = [ "nvidia" ];
-
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
   hardware.nvidia = {
-    open = false;
-    nvidiaSettings = true;
-    modesetting.enable = false;
+    modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
-    forceFullCompositionPipeline = true;
+    open = false;
+    nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
     prime = {
       sync.enable = true;
       allowExternalGpu = true;
-      nvidiaBusId = "PCI:100:0:0";
-      amdgpuBusId = "PCI:193:0:0";
+      amdgpuBusId = "PCI:${amdgpuBusIdHex}";
+      nvidiaBusId = "PCI:${nvidiaBusIdHex}";
     };
   };
-
-  nixpkgs.overlays = [
-    (self: super: {
-      dock = pkgs.writeScriptBin "dock" ''
-        #!${pkgs.stdenv.shell}
-        for i in $(seq 1 60); do
-          echo 1 | tee /sys/bus/pci/rescan
-          sleep 1
-        done
-      '';
-    })
-    (self: super: {
-      undock = pkgs.writeScriptBin "undock" ''
-        #!${pkgs.stdenv.shell}
-        echo 1 | tee /sys/bus/pci/devices/0000:${egpu_pci_id}/remove
-      '';
-    })
-  ];
-
-  security.sudo = {
-    extraConfig = ''
-      ${USER} ALL=(ALL) NOPASSWD: /run/current-system/sw/bin/dock
-      ${USER} ALL=(ALL) NOPASSWD: /run/current-system/sw/bin/undock
-    '';
-  };
-
-  environment.systemPackages = with pkgs; [
-    dock
-    undock
-  ];
 }
